80b9d9a0e6866142aef2c3a5a5b97638a6906109
Albina <olifirenko.albina@gmail.com>
2022-11-28 10:34:22 +0300

    Add connection between IBOutlet and storyboard's elements New func in ResultViewController

--- Personal Quiz/ViewControllers/ResultViewController.swift
+++ Personal Quiz/ViewControllers/ResultViewController.swift
@@ -9,23 +9,42 @@ import UIKit
 
 class ResultViewController: UIViewController {
     
+    @IBOutlet var animalTypeLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
     var answers: [Answer]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
 
-        // Do any additional setup after loading the view.
+    }
+}
+
+//MARK: - Private Methods
+
+extension ResultViewController {
+    private func updateResult() {
+        var frequencyOfAnimals: [Animal: Int] = [:]
+        let animals = answers.map{ $0.animal }
+        
+        for animal in animals {
+            if let animalTypeCount = frequencyOfAnimals[animal] {
+                frequencyOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequencyOfAnimals[animal] = 1
+            }
+        }
+        
+        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+        guard let mostFrequencyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequencyAnimal)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func updateUI(with animal: Animal) {
+        animalTypeLabel.text = "Вы - \(animal.rawValue)!"
+        descriptionLabel.text = animal.definition
     }
-    */
-
 }
